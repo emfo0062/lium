4082c82e15a143e5ff01be40baa9359887952a50
Petter <petter@lerenius.se>
2013-12-08 21:32:56 +0100

    New try

--- tema5/5B/my_add_x.c
+++ tema5/5B/my_add_x.c
@@ -2,12 +2,13 @@
 
 int main (void)
 {
+  char char_in[10];
   int int_in, result;
   int sum = 0;
-  char in[100];
-  fgets(in,sizeof in,stdin);
-  while (sscanf(in,"%d",&int_in))
+  while (scanf("%s",&char_in[0]))
   {
+    if (char_in[0] == '\n') { printf("Result: %d\n",sum);return 0; }
+    sscanf(char_in,"%d",&int_in);
     printf("%3d + %d = %3d\n", sum, int_in, int_in + sum);
     sum += int_in;
   }
