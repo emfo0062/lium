11a31ad3afa815357fb13376384960eddf2d76fd
Petter <petter@lerenius.se>
2014-03-20 22:22:14 +0100

    Start to read lines from files

--- tema10/print2lines.py
+++ tema10/print2lines.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
+import os
 
-print "hej"
+dirPath = "."
+print "print2lines will now print first 2 lines of each file in the directory " + dirPath
+print "--------------------------------------"
+
+for file in os.listdir(dirPath):
+    if file.endswith(".txt"):
+      fh = open(file)
+      line1 = fh.readline().rstrip()
+      line2 = fh.readline().rstrip()
+      fh.close()
+      print file + ":"
+      print "1: " + line1
+      print "2: " + line2
+      print "--------------------------------------"
+
+print "That's all for now folks..."
